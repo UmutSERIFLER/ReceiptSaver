635f906cc16399b777aea73df20f0cea05fbcab9
Umut ŞERİFLER <umut.serifler@sallinggroup.com>
2021-04-21 12:14:15 +0200

    -- type error fixed -- removed unused code block

--- ReceiptSaver/ReceiptSaver/Extensions/UIAlertController+Extension.swift
+++ ReceiptSaver/ReceiptSaver/Extensions/UIAlertController+Extension.swift
@@ -10,7 +10,7 @@ import UIKit
 extension UIAlertController {
     
     static func make(_ title: String = "",message: String , _ doneTitle: String = "Approve",_ cancelTitle: String = "Cancel", placeHolder: String = "", completionHandler: @escaping (String?)->()) -> UIAlertController {
-        let alertController = UIAlertController(title: title, message: "Enter value for \(message)", preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: title, message: "\(message)", preferredStyle: UIAlertController.Style.alert)
         let createAction = UIAlertAction(title: doneTitle, style: UIAlertAction.Style.default) { (action) -> Void in
             DispatchQueue.main.async {
                 completionHandler(alertController.textFields?.first?.text)
@@ -19,12 +19,7 @@ extension UIAlertController {
         
         alertController.addAction(createAction)
         alertController.addAction(UIAlertAction(title: cancelTitle, style: UIAlertAction.Style.cancel, handler: nil))
-        alertController.addTextField { (textField) -> Void in
-            textField.placeholder = placeHolder
-            DispatchQueue.main.async {
-                print(textField)
-            }
-        }
+        alertController.addTextField(configurationHandler: nil)
         return alertController
     }
     
